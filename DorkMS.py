4e4aa635f8a8e85d78e682c0cd2aadbf8efa9aa9
RHYru9 <104529423+rhymsc@users.noreply.github.com>
2023-11-15 20:45:19 +0700

    Update DorkMS.py

--- DorkMS.py
+++ DorkMS.py
@@ -1,30 +1,25 @@
-#!/usr/bin/env/python3
+# !/usr/bin/env/python3
 # This Python file uses the following encoding: utf-8
 
 from __future__ import print_function
 try:
     from googlesearch import search
-
+    import tldextract
 except ImportError:
     print("")
 
 import sys
 import time
 
-
-
-
 if sys.version[0] in "2":
-    print ("\n[x] Not Supported For python 2.x Use Python 3.x \n")
-    print ("\n\n\tRhyRU9 \033[1;91mI LOOTING \033[0m😃\n\n")
+    print("\n[x] Not Supported For python 2.x Use Python 3.x \n")
+    print("\n\n\tRhyRU9 \033[1;91mI LOOTING \033[0m😃\n\n")
     exit()
-
 
 class colors:
     CRED2 = "\33[91m"
     CBLUE2 = "\33[94m"
     ENDC = "\033[0m"
-
 
 banner = ("""
           ______ _          ______ _   _  _____  
@@ -36,7 +31,6 @@ banner = ("""
                         __/ |                    
                        |___/                     
 """)
-
 
 for col in banner:
     print(colors.CRED2 + col, end="")
@@ -62,19 +56,17 @@ for col in z:
     sys.stdout.flush()
     time.sleep(0.4)
 
-
 try:
     data = input("\n[+] Do You Like To Save The Output In A File? (Y/N) ").strip()
     l0g = ("")
 
 except KeyboardInterrupt:
-        print ("\n")
-        print ("\033[1;91m[!] Interruption..!\033[0")
+        print("\n")
+        print("\033[1;91m[!] Interruption..!\033[0")
         time.sleep(0.5)
-        print ("\n\n\t\033[1;91m[!] use sparingly \033[0m\n\n")
+        print("\n\n\t\033[1;91m[!] use sparingly \033[0m\n\n")
         time.sleep(0.5)
         sys.exit(1)
-
 
 def logger(data):
     file = open((l0g) + ".txt", "a")
@@ -82,49 +74,52 @@ def logger(data):
     file.write("\n")
     file.close()
 
-
 if data.startswith("y" or "Y"):
     l0g = input("[~] Give The File a Name: ")
-    print ("\n" + "  " + "»" * 78 + "\n")
+    print("\n" + "  " + "»" * 78 + "\n")
     logger(data)
 else:
-    print ("[!] Saving is Skipped...")
-    print ("\n" + "  " + "»" * 78 + "\n")
-
+    print("[!] Saving is Skipped...")
+    print("\n" + "  " + "»" * 78 + "\n")
 
 def dorks():
     try:
         dork = input("\n[+] Dork Search Query: ")
         amount = input("[+] Number Of Websites To Display: ")
-        print ("\n ")
+        print("\n ")
 
         requ = 0
 
-        for results in search(dork, tld="com", lang="en", num=int(amount), start=0, stop=None, pause=2):
-            print (results)
+        for result in search(dork, tld="com", lang="en", num=int(amount), start=0, stop=None, pause=2):
+            ext = tldextract.extract(result)
+            if ext.subdomain:
+                full_url = f"https://{ext.subdomain}.{ext.domain}.{ext.suffix}"
+            else:
+                full_url = f"http://{ext.domain}.{ext.suffix}"
+            
+            print(full_url)
             time.sleep(0.1)
             requ += 1
             if requ >= int(amount):
                 break
 
-            data = (results)
+            data = (full_url)
 
             logger(data)
             time.sleep(0.1)
 
     except KeyboardInterrupt:
-            print ("\n")
-            print ("\033[1;91m[!]Interruption..!\033[0")
+            print("\n")
+            print("\033[1;91m[!] Interruption..!\033[0")
             time.sleep(0.5)
-            print ("\n\n\t\033[1;91m[!] use sparingly \033[0m\n\n")
+            print("\n\n\t\033[1;91m[!] use sparingly \033[0m\n\n")
             time.sleep(0.5)
             sys.exit(1)
 
-    print ("[•] Done... Exiting...")
-    print ("\n\t\t\t\t\033[34mRhyRU9\033[0m")
-    print ("\t\t\033[1;91m[!] use sparingly \033[0m\n\n")
+    print("[•] Done... Exiting...")
+    print("\n\t\t\t\t\033[34mRhyRU9\033[0m")
+    print("\t\t\033[1;91m[!] use sparingly \033[0m\n\n")
     sys.exit()
-
 
 # =====# Main #===== #
 if __name__ == "__main__":
